a943751c0761962ed9460e836c9187b3ecebd4c9
Dimas Alves Pereira <dimasdevspro@gmail.com>
2024-05-08 16:34:29 -0300

    developed App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import wallet from '../src/img/wallet.svg'
+import login from '../src/img/login.svg'
+import register from '../src/img/register.svg'
 
 function App() {
   return (
-    <div>
-      <p>Wallet for Sellers</p>
-    </div>
+    <main>
+      <h1>E-WALLET</h1><h4>FOR SELLERS</h4>
+      <img src={wallet}/>
+      <section>
+        <div>
+          <img src={login}/>
+          <p>Login</p>
+        </div>
+        <div>
+          <img src={register}/>
+          <p>Register</p>
+        </div>
+      </section>
+    </main>
   );
 }
 
