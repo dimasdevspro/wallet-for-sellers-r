c9a71deb753e170041ba4673b966e807882ab19f
Dimas Alves Pereira <dimasdevspro@gmail.com>
2024-05-17 16:03:08 -0300

    developed routes login and register

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import wallet from '../src/img/wallet.svg'
 import login from '../src/img/login.svg'
 import register from '../src/img/register.svg'
-
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import Login from '../src/components/pages/Login'
+import Register from '../src/components/pages/Register'
 import './App.css'
 
 function App() {
   return (
     <main>
       <h1>E-WALLET</h1><h4>FOR SELLERS</h4>
-      <img src={wallet}/>
+      <img src={wallet} alt='wallet'/>
+      <Router>
       <section>
-        <div>
-          <img className='access' src={login}/>
+        <Link to='/login'>
+        <img className='access' src={login} alt='login'/>
           <p className='access'>Login</p>
-        </div>
-        <div>
-          <img className='access' src={register}/>
+        </Link>
+        <Link to='/register'>
+          <img className='access' src={register} alt='register'/>
           <p className='access'>Register</p>
-        </div>
+        </Link>
       </section>
+      <Routes>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
+      </Router>
     </main>
   );
 }
