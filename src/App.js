a100dbf057dde9fe0d54ad9f35a354f99fb32641
Dimas Alves Pereira <dimasdevspro@gmail.com>
2024-05-28 13:29:52 -0300

    developed button with useNavigate

--- src/App.js
+++ src/App.js
@@ -1,33 +1,44 @@
 import wallet from '../src/img/wallet.svg'
 import login from '../src/img/login.svg'
 import register from '../src/img/register.svg'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import { useNavigate, BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Login from '../src/components/pages/Login'
 import Register from '../src/components/pages/Register'
 import './App.css'
 
 function App() {
+  const navigate = useNavigate()
+ 
+  function handleClickLogin(){
+    navigate('/login')
+  }
+
+  function handleClickRegister(){
+    navigate('/register')
+  }
+
   return (
+    <Router>
+    
     <main>
       <h1>E-WALLET</h1><h4>FOR SELLERS</h4>
       <img src={wallet} alt='wallet'/>
-      <Router>
       <section>
-        <Link to='/login' className='access'>
+        <div>
         <img src={login} alt='login'/>
-          <p>Login</p>
-        </Link>
-        <Link to='/register' className='access'>
-          <img src={register} alt='register'/>
-          <p>Register</p>
-        </Link>
+          <Link onClick={handleClickLogin}>Login</Link>
+        </div>
+        <div>
+        <img src={register} alt='register'/>
+          <Link onClick={handleClickRegister}>Register</Link>   
+        </div>
       </section>
+    </main>
       <Routes>
         <Route path='/login' element={<Login/>}/>
         <Route path='/register' element={<Register/>}/>
       </Routes>
-      </Router>
-    </main>
+    </Router>
   );
 }
 
