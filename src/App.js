df1a0c41c0f355b5382ac364790fa21c086ec37f
Dimas Alves Pereira <dimasdevspro@gmail.com>
2024-05-28 15:38:37 -0300

    completely reestrutured project

--- src/App.js
+++ src/App.js
@@ -1,42 +1,24 @@
-import wallet from '../src/img/wallet.svg'
-import login from '../src/img/login.svg'
-import register from '../src/img/register.svg'
-import { useNavigate, BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from '../src/components/pages/Home'
 import Login from '../src/components/pages/Login'
 import Register from '../src/components/pages/Register'
+import Company from '../src/components/pages/Company'
+import Contact from '../src/components/pages/Contact'
+import Navbar from './components/layout/Navbar'
 import './App.css'
 
 function App() {
-  const navigate = useNavigate()
  
-  function handleClickLogin(){
-    navigate('/login')
-  }
-
-  function handleClickRegister(){
-    navigate('/register')
-  }
-
   return (
+
     <Router>
-    
-    <main>
-      <h1>E-WALLET</h1><h4>FOR SELLERS</h4>
-      <img src={wallet} alt='wallet'/>
-      <section>
-        <div>
-        <img src={login} alt='login'/>
-          <Link onClick={handleClickLogin}>Login</Link>
-        </div>
-        <div>
-        <img src={register} alt='register'/>
-          <Link onClick={handleClickRegister}>Register</Link>   
-        </div>
-      </section>
-    </main>
+      <Navbar/>
       <Routes>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/register' element={<Register/>}/>
+      <Route exact path='/' element={<Home/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/company' element={<Company/>}/>
+      <Route path='/contact' element={<Contact/>}/>
       </Routes>
     </Router>
   );
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+import wallet from '../../img/wallet.svg';
+import styles from '../layout/Navbar.module.css'
+
+function Navbar(){
+    return (
+        <nav className={styles.navbar}>
+                <img src={wallet} alt='wallet'/>
+            <ul className={styles.list}>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/login'>Login</Link></li>
+                <li><Link to='/register'>Register</Link></li>
+                <li><Link to='/company'>Company</Link></li>
+                <li><Link to='/contact'>Contact</Link></li>
+
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
