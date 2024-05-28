6c3115621a84a360092a7db4554afadf1c7cb792
Dimas Alves Pereira <dimasdevspro@gmail.com>
2024-05-28 16:33:41 -0300

    add footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Register from '../src/components/pages/Register'
 import Company from '../src/components/pages/Company'
 import Contact from '../src/components/pages/Contact'
 import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 import './App.css'
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
       <Route path='/company' element={<Company/>}/>
       <Route path='/contact' element={<Contact/>}/>
       </Routes>
+      <Footer/>
     </Router>
   );
 }
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,24 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import styles from '../layout/Footer.module.css';
+
+function Footer(){
+    return (
+        
+        <footer className={styles.footer}>
+            <ul className={styles.list}>
+            <li>
+               <a href='https://www.facebook.com/dimas.pereira.547' target='blank'><FaFacebook/></a>
+            </li>
+            <li>
+               <a href='https://www.instagram.com/dimas_apereira/' target='blank'><FaInstagram/></a>
+            </li>
+            <li>
+               <a href='https://www.linkedin.com/in/dimas-apereira/' target='blank'><FaLinkedin/></a>
+            </li>
+         </ul>
+           <p> WALLET © 2024</p>
+        </footer>
+    )
+}
+
+export default Footer
