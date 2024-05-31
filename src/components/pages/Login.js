bfa68f2e4465423a073136e43ba1d86dcb79eca2
Dimas Alves Pereira <dimasdevspro@gmail.com>
2024-05-31 16:10:54 -0300

    developed page login

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,6 +1,24 @@
+import Login2 from '../../img/Login2.svg';
+import Key from '../../img/Key.svg';
+import Button from '../../img/Button.svg';
+
+import styles from './Login.module.css';
+
 function Login(){
     return (
-        <div>Login</div>
+        <div className={styles.body}>
+            <form method="post" action="./login">
+                <label className={styles.sr_only} for="login-seller">Login</label>
+                <img src={Login2} alt='Login'/>
+            <input type='text' name='login' id='login-seller' placeholder='input your nickname'/>
+                <label className={styles.sr_only} for="password-seller">Password</label>
+                <img src={Key} alt='password'/>
+            <input type='password' name='password' id='password-seller' placeholder='input your password'/> 
+            <button>
+                <img src={Button} alt='button login'/>    
+            </button>   
+            </form>
+        </div>
     )
 }
 
