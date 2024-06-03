3475baa54637c2554ba8e1adbc66dac5c10e540f
Dimas Alves Pereira <dimasdevspro@gmail.com>
2024-06-03 16:23:40 -0300

    developed style´s form

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,4 +1,4 @@
-import Login2 from '../../img/Login2.svg';
+import login from '../../img/login.svg';
 import Key from '../../img/Key.svg';
 import Button from '../../img/Button.svg';
 
@@ -7,13 +7,17 @@ import styles from './Login.module.css';
 function Login(){
     return (
         <div className={styles.body}>
-            <form method="post" action="./login">
+            <form  className={styles.form} method="post" action="./login">
+                <div className={styles.input}>
                 <label className={styles.sr_only} for="login-seller">Login</label>
-                <img src={Login2} alt='Login'/>
-            <input type='text' name='login' id='login-seller' placeholder='input your nickname'/>
+                <img src={login} alt='Login'/>
+                <input type='text' name='login' id='login-seller' placeholder='input your nickname'/>
+                </div>
+                <div className={styles.input}>
                 <label className={styles.sr_only} for="password-seller">Password</label>
                 <img src={Key} alt='password'/>
-            <input type='password' name='password' id='password-seller' placeholder='input your password'/> 
+            <   input type='password' name='password' id='password-seller' placeholder='input your password'/> 
+                </div>
             <button>
                 <img src={Button} alt='button login'/>    
             </button>   
