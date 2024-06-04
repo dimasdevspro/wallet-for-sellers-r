54239209bdaf0311c23de8fcdb9ad0b6d829191c
Dimas Alves Pereira <dimasdevspro@gmail.com>
2024-06-04 16:56:26 -0300

    developed page contact

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,13 @@
+import styles from './Contact.module.css';
+
 function Contact(){
     return (
-        <div>Contact</div>
+        <div className={styles.body}>
+            <h1>Contact</h1>
+            <p>Address: Rua São Constantino, 207 - Vila Santa Cruz</p>
+            <p>CEP 04456-000 - São Paulo - S.P.</p>
+            <p>Phone: +55(011)93222-8112</p>
+        </div>
     )
 }
 
