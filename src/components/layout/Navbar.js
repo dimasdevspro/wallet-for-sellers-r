8766addf7c36cd57d92e67a6228ba3b623464b2d
Dimas Alves Pereira <dimasdevspro@gmail.com>
2024-06-04 16:20:31 -0300

    modified company for app in navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -10,7 +10,7 @@ function Navbar(){
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to='/login'>Login</Link></li>
                 <li><Link to='/register'>Register</Link></li>
-                <li><Link to='/company'>Company</Link></li>
+                <li><Link to='/company'>App</Link></li>
                 <li><Link to='/contact'>Contact</Link></li>
 
             </ul>
