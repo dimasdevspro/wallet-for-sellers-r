7e6809a6164c9e81696eb5e88b712a7c3da6a64e
Dimas Alves Pereira <dimasdevspro@gmail.com>
2024-06-04 16:40:56 -0300

    describe about app

--- src/components/pages/App.js
+++ src/components/pages/App.js
@@ -1,6 +1,11 @@
+import styles from './App.module.css';
+
 function App(){
     return (
-        <div>Company</div>
+        <div className={styles.body}>
+        <h1>About App</h1>
+        <p>Developed by Devspro Solution, this application aims to help sellers register their customers in a single, simple and fast note-taker.</p>
+        </div>
     )
 }
 
