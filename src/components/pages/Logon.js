a5c8ca661cf27202bb5b819aee9ee7d5a23f510d
Dimas Alves Pereira <dimasdevspro@gmail.com>
2024-06-06 16:09:12 -0300

    added elements by page´s logon

--- src/components/pages/Logon.js
+++ src/components/pages/Logon.js
@@ -1,7 +1,29 @@
+import styles from './Logon.module.css';
+
+import Seller from '../../img/login.svg'
+import Costumers from '../../img/Costumers.svg'
+import Addcostumers from '../../img/Addcostumers.svg'
 
 function Logon() {
     return(
-        <div>Logon</div>
+        <div>
+            <form method='' action=''>
+                <img src={Seller} alt="seller"/>
+                <h1>Seller</h1>
+                <label className={styles.sr_only} for='seller'>Seller</label>
+                <input type='text' name='seller' id='seller'/>
+            </form>
+            <div>
+                <div>
+                    <img src={Costumers} alt='Costumers'/>
+                    <p>Costumers</p>
+                </div>
+                <div>
+                    <img src={Addcostumers} alt='Add costumers'/>
+                    <p>Add Costumers</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
