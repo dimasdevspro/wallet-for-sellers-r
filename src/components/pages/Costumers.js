c2570e945c53a99268cd644eed8f2976ab9ec94b
Dimas Alves Pereira <dimasdevspro@gmail.com>
2024-06-10 15:18:39 -0300

    added form by costumers´s page

--- src/components/pages/Costumers.js
+++ src/components/pages/Costumers.js
@@ -1,12 +1,18 @@
 import { Link } from "react-router-dom"
 
+import Seller from '../../img/login.svg'
+
+import styles from './Costumers.module.css'
+
 function Costumers(){
     return (
-        <div>
-            List Costumers
-            <ul>
-                <li><Link to='/costumer-info'>Costumer info</Link></li>
-            </ul>
+        <div >
+            <form method='' action='' >
+                <img src={Seller} alt="seller"/>
+                <h1>Seller</h1>
+                <label className={styles.sr_only} for='seller'>Seller</label>
+                <input type='text' name='seller' id='seller'/>
+            </form>
         </div>
     )
 }
