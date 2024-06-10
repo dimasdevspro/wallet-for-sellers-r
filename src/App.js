228e6265c9f2dec15e693381189d8c0185da17cb
Dimas Alves Pereira <dimasdevspro@gmail.com>
2024-06-10 13:32:00 -0300

    add route by 3 pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Register from '../src/components/pages/Register'
 import App1 from '../src/components/pages/App'
 import Contact from '../src/components/pages/Contact'
 import Logon from '../src/components/pages/Logon'
+import Costumers from '../src/components/pages/Costumers'
+import CostumerInfo from '../src/components/pages/CostumerInfo'
+import CostumerAdd from '../src/components/pages/CostumerAdd'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import './App.css'
@@ -22,7 +25,9 @@ function App() {
       <Route path='/company' element={<App1/>}/>
       <Route path='/contact' element={<Contact/>}/>
       <Route path='/logon' element={<Logon/>}/>
-
+      <Route path='/costumers' element={<Costumers/>}/>
+      <Route path='/costumer-info' element={<CostumerInfo/>}/>
+      <Route path='/costumer-add' element={<CostumerAdd/>}/>
       </Routes>
       <Footer/>
     </Router>
