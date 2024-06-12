2040c66174ea73506228a53217d27cd6adac6c0e
Dimas Alves Pereira <dimasdevspro@gmail.com>
2024-06-12 14:51:51 -0300

    input div in form´s costumer-add

--- src/components/pages/CostumerAdd.js
+++ src/components/pages/CostumerAdd.js
@@ -6,31 +6,39 @@ import styles from './CostumerAdd.module.css';
 
 function CostumerAdd(){
     return(
-        <div>
+        <div className={styles.container}>
             <div>
                 <img src={AddCostumer}/>
                 <h1>Add Costumer</h1>
             </div>
-        <form method="" action="">
-            <p>Name</p>
-            <label className={styles.sr_only} for="name-costumer">Name</label>
-        <input type="text" name="name-costumeer" placeholder="insert to your name" required/>
-            <p>Email</p>
-            <label className={styles.sr_only} for="email-costumer">Email</label>
-        <input type="email" name="email-costumer" placeholder="insert to your email" required/>
-            <p>Phone</p>
-            <label className={styles.sr_only} for="phone-costumer">Phone</label>
-        <input type="tel" name="phone-costumer" id="phone-costumer" 
-        placeholder="+xx(xx)xxxxxxxxx"
-        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"/>
-            <p>Informations</p>
-            <textarea name="informations" id="infos" ></textarea>
-            <button>
-                <img  src={Costumers} alt="Button for list costumers"/>
-            </button>
-            <button >
-            <img src={Save} alt="Button for save new costumer"/>
-        </button>
+        <form method="" action="" className={styles.form}>
+            <div>
+                <label className={styles.sr_only} for="name-costumer">Name</label>
+                <p>Name</p>
+                <input type="text" name="name-costumeer" placeholder="insert to your name" required/>
+            </div>
+            <div>
+                <label className={styles.sr_only} for="email-costumer">Email</label>
+                <p>Email</p>
+                <input type="email" name="email-costumer" placeholder="insert to your email" required/>
+            </div>    
+            <div>
+                <label className={styles.sr_only} for="phone-costumer">Phone</label>
+                <p>Phone</p>
+                <input type="tel" name="phone-costumer" id="phone-costumer" 
+                        placeholder="+xx(xx)xxxxxxxxx"
+                        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"/>
+            </div>
+            <div>
+                <p>Informations</p>
+                <textarea name="informations" id="infos" ></textarea>
+            </div>
+                <button>
+                    <img  src={Costumers} alt="Button for list costumers"/>
+                </button>
+                <button >
+                    <img src={Save} alt="Button for save new costumer"/>
+                </button>
         </form>
         </div>
     )
