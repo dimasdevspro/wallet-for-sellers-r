a93b248ab2b0f55d0291b188dd5a6664732f902f
Dimas Alves Pereira <dimasdevspro@gmail.com>
2024-06-16 18:57:36 -0300

    adjusted form register seller

--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -3,34 +3,36 @@ import styles from './Register.module.css';
 import save from '../../img/save.svg';
 
 function Register(){
+
+    
     return(
         <div className={styles.body}>
             <h1>Register´s Seller</h1>
-            <form  className={styles.form} method="post" action="./register">
+            <form  className={styles.form}>
             <div className={styles.input}>
-                <label className={styles.sr_only} for="name-seller">Name</label>
+                <label className={styles.sr_only}>Name</label>
                 <p>Name</p>
-                <input type='text' name='name' id='name-seller' placeholder='input your name'/>
+                <input type='text' name='name' placeholder='input your name'/>
                 </div>
                 <div className={styles.input}>
-                <label className={styles.sr_only} for="email-seller">Email</label>
+                <label className={styles.sr_only}>Email</label>
                 <p>Email</p>
-                <input type='email' name='email' id='email-seller' placeholder='input your email'/>
+                <input type='email' name='email' placeholder='input your email'/>
                 </div>
                 <div className={styles.input}>
-                <label className={styles.sr_only} for="phone-seller">Phone</label>
+                <label className={styles.sr_only}>Phone</label>
                 <p>Phone</p>
-                <input type='tel' name='tel' id='phone-seller' placeholder='input your phone or cel'/>
+                <input type='tel' name='tel' placeholder='input your phone or cel'/>
                 </div>
                 <div className={styles.input}>
-                <label className={styles.sr_only} for="login-seller">Login</label>
+                <label className={styles.sr_only}>Login</label>
                 <p>Login</p>
-                <input type='text' name='login' id='login-seller' placeholder='input your login'/>
+                <input type='text' name='login' placeholder='input your login'/>
                 </div>
                 <div className={styles.input}>
-                <label className={styles.sr_only} for="password-seller">Password</label>
+                <label className={styles.sr_only}>Password</label>
                 <p>Password</p>
-                <input type='password' name='password' id='password-seller' placeholder='input your password'/> 
+                <input type='password' name='password' placeholder='input your password'/> 
                 </div>
             <button>
                 <img src={save} alt='button login'/>    
