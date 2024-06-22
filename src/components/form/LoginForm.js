efa67f20ca3475a4124ead7e9415d87e1a889f95
Dimas Alves Pereira <dimasdevspro@gmail.com>
2024-06-22 17:22:27 -0300

    refatored form´s login in component

---
+++ src/components/form/LoginForm.js
@@ -0,0 +1,36 @@
+import styles from './LoginForm.module.css';
+
+import login from '../../img/login.svg';
+import Key from '../../img/Key.svg';
+import Button from '../../img/Button.svg';
+import Input from '../form/Input';
+
+function LoginForm({ handleSubmit, sellerData }){
+    return (
+        <form  className={styles.form}>
+                <div className={styles.input}>
+                <label className={styles.sr_only}>Login</label>
+                <img src={login} alt='Login'/>
+                <Input
+                type='text' 
+                name='login' 
+                placeholder='input your login'
+                />
+                </div>
+                <div className={styles.input}>
+                <label className={styles.sr_only}>Password</label>
+                <img src={Key} alt='password'/>
+                <Input
+                type='password'
+                name='password' 
+                placeholder='input your password'
+                /> 
+                </div>
+            <button>
+                <img src={Button} alt='button login'/>    
+            </button>   
+            </form>
+    )
+}
+
+export default LoginForm
--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,7 +1,5 @@
-import login from '../../img/login.svg';
-import Key from '../../img/Key.svg';
-import Button from '../../img/Button.svg';
 
+import LoginForm from '../form/LoginForm';
 import styles from './Login.module.css';
 
 function Login(){
@@ -23,21 +21,7 @@ function Login(){
     return (
         <div className={styles.body}>
             <h1>Login´s Seller</h1>
-            <form  className={styles.form} method="" action="./logon">
-                <div className={styles.input}>
-                <label className={styles.sr_only} for="login-seller">Login</label>
-                <img src={login} alt='Login'/>
-                <input type='text' name='login' id='login-seller' placeholder='input your login'/>
-                </div>
-                <div className={styles.input}>
-                <label className={styles.sr_only} for="password-seller">Password</label>
-                <img src={Key} alt='password'/>
-            <   input type='password' name='password' id='password-seller' placeholder='input your password'/> 
-                </div>
-            <button>
-                <img src={Button} alt='button login'/>    
-            </button>   
-            </form>
+            <LoginForm handleSubmit={getLogin}/>
         </div>
     )
 }
