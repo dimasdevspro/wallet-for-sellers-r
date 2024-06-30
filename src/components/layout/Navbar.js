d048ce1c292327c1737702263573809b5879ed1f
Dimas Alves Pereira <dimasdevspro@gmail.com>
2024-06-30 17:40:45 -0300

    add logic in menu when is logon

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,14 +1,19 @@
+import { useContext } from 'react';
+
 import { Link } from 'react-router-dom';
 import wallet from '../../img/wallet.svg';
 import styles from '../layout/Navbar.module.css'
+import { AuthContext } from '../../context/AuthContext';
 
 function Navbar(){
+
+    const { auth, setAuth } = useContext(AuthContext)
     return (
         <nav className={styles.navbar}>
                 <img src={wallet} alt='wallet'/>
             <ul className={styles.list}>
                 <li><Link to='/'>Home</Link></li>
-                <li><Link to='/login'>Login</Link></li>
+                {!auth ? <li><Link to='/login'>Login</Link></li> : <li><Link to='/logon'>Logon</Link></li>}
                 <li><Link to='/register'>Register</Link></li>
                 <li><Link to='/company'>App</Link></li>
                 <li><Link to='/contact'>Contact</Link></li>
--- src/components/layout/Navbar2.js
+++ src/components/layout/Navbar2.js
@@ -1,3 +1,4 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import wallet from '../../img/wallet.svg';
 import styles from '../layout/Navbar2.module.css'
@@ -5,6 +6,7 @@ import styles from '../layout/Navbar2.module.css'
 import { FiAlignJustify } from "react-icons/fi";
 
 import { useState } from 'react';
+import { AuthContext } from '../../context/AuthContext';
 
 function Navbar2(){
 
@@ -13,13 +15,15 @@ function Navbar2(){
         setActive(!isActive);
     }; 
 
+    const {auth, setAuth } = useContext(AuthContext)
+
     return (
         <nav className={styles.navbar}>
                 <img src={wallet} alt='wallet' />
                 <FiAlignJustify onClick={ToggleClass}/>
             <ul className={isActive ? styles.list_unvisible : styles.list_visible }>
                 <li><Link to='/'>Home</Link></li>
-                <li><Link to='/login'>Login</Link></li>
+                {!auth ? <li><Link to='/login'>Login</Link></li> : <li><Link to='/logon'>Logon</Link></li>}
                 <li><Link to='/register'>Register</Link></li>
                 <li><Link to='/company'>App</Link></li>
                 <li><Link to='/contact'>Contact</Link></li>
